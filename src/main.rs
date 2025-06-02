31cf266ecfbf8e1a515011d3aace60a07f891bef
krithik <krithiksrinivas200@gmail.com>
2025-06-02 13:47:46 +0000

    server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+//imports 
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(mut stream: TcpStream){
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).expect("Failed to read from client");
+    //this line converts the data in the buffer into utf-8 encoded string
+    let request = String::from_utf8_lossy(&buffer[..]);
+    println!("recieved request: {}",request);
+    let response = "hello client".as_bytes();
+    stream.write(response).expect("failed to write response");
+}
+
+fn main(){
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("failed to bind address");
+    println!("server listening on 127.0.0.1:8080");
+
+    for stream in listener.incoming(){
+        match stream{
+            Ok(stream) => {
+                std::thread::spawn(|| handle_client(stream));
+            }
+            Err(e) => {
+                eprintln!("failed to establish connection: {}", e);
+            }
+        }
+    }
 }
